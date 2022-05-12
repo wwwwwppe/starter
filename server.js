c7ad4547d1ee5056ef70256f4a168bae26b0c7d3
wwwwwppe <64912387+wwwwwppe@users.noreply.github.com>
2022-05-12 20:33:50 +0800

    $git pull origin masker --allow-unrelated-histories

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
 const app = require('./app');
 // console.log(process.env);
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABSE.replace('<PASSWORD>')
+
+mongoose.connect();
 
 const port =process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
+
