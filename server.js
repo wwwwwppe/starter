1f79c86b34b458c5d7782cbb758f07e3b4508c89
wwwwwppe <64912387+wwwwwppe@users.noreply.github.com>
2022-05-13 15:00:03 +0800

    $git pull origin masker --allow-unrelated-histories

--- server.js
+++ server.js
@@ -4,11 +4,20 @@ const app = require('./app');
 // console.log(process.env);
 dotenv.config({ path: './config.env' });
 
-const DB = process.env.DATABSE.replace('<PASSWORD>')
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD); //后面的replace是用DATABASE_PASSWORD代替DATABASE中的PASSWORD这样安全
 
-mongoose.connect();
+mongoose
+  // .connect(process.env.DATABASE_LOCAL,{    // 连接本地数据库
+  .connect(DB, {                      //连接网络数据库
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connection successful'));
 
-const port =process.env.PORT || 3000;
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
